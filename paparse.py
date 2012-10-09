e055c757053020fcde44fd961b432f132006ccfe
Luke GB <git@lukegb.com>
2012-10-09 23:24:14 +0200

    Make paparse.py read files in a line at a time
    Retain backwards compatibility, make cheese and boogy on down.

--- paparse.py
+++ paparse.py
@@ -43,44 +43,49 @@ S_WAITING_DICT_NAME = 2
 def new_ctx():
     return dict(current_key="", tree=dict())
 
-def parse_tree(s):
-    # s is a string containing data to parse
+def parse_tree(sq):
+    # retain backwards compatibility if needed
+    if type(sq) is str:
+        sq = (sq,)
+
     tree_depth = 0
     state = S_WAITING_KEY
     ctx = [new_ctx()]
     cur_ctx = ctx[0]
-    while True:
-        # find key: first non-whitespace char
-        token_value, s = extract_token(s)
-
-        # None is the "no more tokens" sentinel value
-        if token_value is None:
-            break
-
-        if state is S_WAITING_KEY and token_value == "BEGIN":
-            state = S_WAITING_DICT_NAME
-        elif state is S_WAITING_KEY and token_value == "END":
-            old_ctx = ctx.pop(0)
-            cur_ctx = ctx[0]
-            if cur_ctx["current_key"] in cur_ctx["tree"]:
-                if type(cur_ctx["tree"][cur_ctx["current_key"]]) is not list:
-                    cur_ctx["tree"][cur_ctx["current_key"]] = [cur_ctx["tree"][cur_ctx["current_key"]], old_ctx["tree"]]
+    for s in sq:
+        # s is a string containing data to parse
+        while True:
+            # find key: first non-whitespace char
+            token_value, s = extract_token(s)
+    
+            # None is the "no more tokens" sentinel value
+            if token_value is None:
+                break
+    
+            if state is S_WAITING_KEY and token_value == "BEGIN":
+                state = S_WAITING_DICT_NAME
+            elif state is S_WAITING_KEY and token_value == "END":
+                old_ctx = ctx.pop(0)
+                cur_ctx = ctx[0]
+                if cur_ctx["current_key"] in cur_ctx["tree"]:
+                    if type(cur_ctx["tree"][cur_ctx["current_key"]]) is not list:
+                        cur_ctx["tree"][cur_ctx["current_key"]] = [cur_ctx["tree"][cur_ctx["current_key"]], old_ctx["tree"]]
+                    else:
+                        cur_ctx["tree"][cur_ctx["current_key"]].append(old_ctx["tree"])
                 else:
-                    cur_ctx["tree"][cur_ctx["current_key"]].append(old_ctx["tree"])
-            else:
-                cur_ctx["tree"][cur_ctx["current_key"]] = old_ctx["tree"]
-        elif state is S_WAITING_DICT_NAME:
-            cur_ctx["current_key"] = token_value
-            state = S_WAITING_KEY
-            ctx.insert(0, new_ctx())
-            cur_ctx = ctx[0]
-        elif state is S_WAITING_KEY:
-            cur_ctx["current_key"] = token_value
-            state = S_WAITING_VALUE
-        elif state is S_WAITING_VALUE:
-            cur_ctx["tree"][cur_ctx["current_key"]] = token_value
-            state = S_WAITING_KEY
-            cur_ctx["current_key"] = None 
+                    cur_ctx["tree"][cur_ctx["current_key"]] = old_ctx["tree"]
+            elif state is S_WAITING_DICT_NAME:
+                cur_ctx["current_key"] = token_value
+                state = S_WAITING_KEY
+                ctx.insert(0, new_ctx())
+                cur_ctx = ctx[0]
+            elif state is S_WAITING_KEY:
+                cur_ctx["current_key"] = token_value
+                state = S_WAITING_VALUE
+            elif state is S_WAITING_VALUE:
+                cur_ctx["tree"][cur_ctx["current_key"]] = token_value
+                state = S_WAITING_KEY
+                cur_ctx["current_key"] = None 
     if tree_depth != 0:
         raise ParseException("Tree not completely unwound")
     return cur_ctx["tree"]
@@ -88,8 +93,7 @@ def parse_tree(s):
 
 def parse_save(f):
     # .prison files are key-value pairs
-    v = f.read()
-    prison = parse_tree(v)
+    prison = parse_tree(f)
     return prison
 
 def parse_save_from_file(fn, cache=True):
